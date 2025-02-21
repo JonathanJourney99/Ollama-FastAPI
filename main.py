d87c431a346aae79180c35e2788b2431529fc2b7
JonathanJourney99 <ijonathan.gomes@gmail>
2025-02-21 22:46:13 +0530

    FastXOllama

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+import ollama
+import os
+
+app = FastAPI()
+
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your frontend URL if needed
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows POST, GET, OPTIONS, etc.
+    allow_headers=["*"],
+)
+# Serve index.html
+@app.get("/", response_class=HTMLResponse)
+def serve_index():
+    with open("static/index.html", "r") as file:
+        return HTMLResponse(content=file.read())
+
+# Request Model
+class PromptRequest(BaseModel):
+    userprompt: str
+
+@app.post("/model-generate")
+def generate_text(request: PromptRequest):
+    resp = ollama.chat(model="qwen2.5:1.5b", messages=[{"role":"user", "content": request.userprompt}])
+    return {"resp": resp['message']['content']}
